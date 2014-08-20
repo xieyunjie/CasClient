f69845f72d5f479e1336837f657987e386c5f0c1
xieyunjie <yunjie.xie@hotmail.com>
2014-08-20 12:08:16 +0800

    添加ajax调用

--- src/main/java/CasServlet/testServlet.java
+++ src/main/java/CasServlet/testServlet.java
@@ -18,34 +18,49 @@ import org.springframework.security.cas.authentication.CasAuthenticationToken;
  * Servlet implementation class testServlet
  */
 @WebServlet("/testServlet")
-public class testServlet extends HttpServlet {
+public class testServlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public testServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 
-		CasAuthenticationToken token = (CasAuthenticationToken)request.getUserPrincipal();
-		List<String> list =  (ArrayList<String>)token.getAssertion().getPrincipal().getAttributes().get("resource");
-		
-		
-//		AttributePrincipal principal = (AttributePrincipal)request.getUserPrincipal();
-//		Map attributes = principal.getAttributes(); 
+	public testServlet()
+	{
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+
+		try
+		{
+			CasAuthenticationToken token = (CasAuthenticationToken) request.getUserPrincipal();
+			List<String> list = (List<String>) token.getAssertion().getPrincipal().getAttributes().get("resource");
+
+			System.out.println(list.get(0));
+		}
+		catch (Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+
+		// AttributePrincipal principal =
+		// (AttributePrincipal)request.getUserPrincipal();
+		// Map attributes = principal.getAttributes();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
 	}
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,11 +1,34 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%
+	String path = request.getContextPath();
+%>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+<script type="text/javascript" src="<%=path%>/resources/js/jquery-2.1.1.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#formbutton').on('click', function(e) {
+			$.ajax({
+				url:'<%=path%>/testServlet',
+				data:{user:'xxx',password:'bbbb'},
+				type:'GET',
+				datetype:'text',
+				success:function(data){
+					alert('xxxx yyyy');
+				}
+			})
+		})
+	});
+</script>
 </head>
 <body>
 	<h1>index1</h1>
+
+	<form action="post">
+		<input type="button" id="formbutton" value="XXXXXX" />
+	</form>
 </body>
 </html>
